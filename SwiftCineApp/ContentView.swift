e01a1b6c4dffec2da977debbe760bd135083bce5
LORSOLD PRADON Alyssia <ap221698@cjkgtdn2lp.edu.atlas.local>
2025-11-05 10:00:18 +0100

    firstcommit

---
+++ SwiftCineApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCineApp
+//
+//  Created by LORSOLD PRADON Alyssia on 05/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCineApp/SwiftCineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCineAppApp.swift
+//  SwiftCineApp
+//
+//  Created by LORSOLD PRADON Alyssia on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
